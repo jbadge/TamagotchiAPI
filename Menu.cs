da0cd83572644a5e5eb8c6d2813fc2f618d55036
Jeremy Badger <jeremebadger@gmail.com>
2023-05-25 18:17:02 -0700

    Menu Errors

--- Menu.cs
+++ Menu.cs
@@ -2,54 +2,53 @@ using System;
 
 namespace TamagotchiAPI
 {
-    public class Menus
+    public class Menu
     {
 
 
-        //         public static void WelcomeMessage()
-        //         {
+        public static void WelcomeMessage()
+        {
 
-        //             //             Console.ForegroundColor = ConsoleColor.White;
-        //             //             Console.WriteLine(@"    
-        //             //             Welcome to...");
-        //             //             Console.WriteLine(@"
-        //             // 888                                                888           888     d8b 
-        //             // 888                                                888           888     Y8P 
-        //             // 888                                                888           888         
-        //             // 888888 8888b. 88888b.d88b.  8888b.  .d88b.  .d88b. 888888 .d8888b88888b. 888 
-        //             // 888       "88b888 "888 "88b    "88bd88P"88bd88""88b888   d88P"   888 "88b888
-        //             // 888.d888888888  888  888.d888888888  888888  888888   888     888  888888
-        //             // Y88b. 888  888888  888  888888  888Y88b 888Y88..88PY88b.Y88b.   888  888888
-        //             //  "Y888"Y888888888  888  888"Y888888 "Y88888 "Y88P"  "Y888 "Y8888P888  888888
-        //             //                                         888
-        //             //                                    Y8b d88P
-        //             //                                     "Y88P"
-        //             //             );
+            //             //             Console.ForegroundColor = ConsoleColor.White;
+            //             //             Console.WriteLine(@"    
+            //             //             Welcome to...");
+            //             //             Console.WriteLine(@"
+            //             // 888                                                888           888     d8b 
+            //             // 888                                                888           888     Y8P 
+            //             // 888                                                888           888         
+            //             // 888888 8888b. 88888b.d88b.  8888b.  .d88b.  .d88b. 888888 .d8888b88888b. 888 
+            //             // 888       "88b888 "888 "88b    "88bd88P"88bd88""88b888   d88P"   888 "88b888
+            //             // 888.d888888888  888  888.d888888888  888888  888888   888     888  888888
+            //             // Y88b. 888  888888  888  888888  888Y88b 888Y88..88PY88b.Y88b.   888  888888
+            //             //  "Y888"Y888888888  888  888"Y888888 "Y88888 "Y88P"  "Y888 "Y8888P888  888888
+            //             //                                         888
+            //             //                                    Y8b d88P
+            //             //                                     "Y88P"
+            //             //             );
 
-        //             //             Console.WriteLine("Press any key to continue...");
-        //             //             Console.ReadKey(true).Key.ToString();
-        //             //             Console.Clear();
+            //             //             Console.WriteLine("Press any key to continue...");
+            //             //             Console.ReadKey(true).Key.ToString();
+            //             //             Console.Clear();
 
 
-        //             Console.ForegroundColor = ConsoleColor.White;
-        //             Console.WriteLine(@"    
-        //             Welcome to...");
-        //             string text = @"
-        //                                                                                           88          88  
-        //   ,d                                                                      ,d              88         \""
-        //   88                                                                      88              88";
-        //             // MM88MMM ,adPPYYba, 88,dPYba,,adPYba,  ,adPPYYba,  ,adPPYb,d8  ,adPPYba, MM88MMM ,adPPYba, 88,dPPYba,  88
-        //             //   88   \"\"    \`Y8 88P\' \"88\"  \"8a\"\"    `Y8 a8\"   `Y88 a8\"   \"8a  88   a8\"   \"\"88P\'  \"8a 88";
-        //             //   88    ,adPPPPP88 88      88      88 ,adPPPPP88 8b       88 8b       d8  88   8b         88       88 88";
-        //             //   88,   88,    ,88 88      88      88 88,    ,88 "8a,   ,d88 "8a,   ,a8"  88   "8a,   ,aa 88       88 88
-        //             //   "Y888 `"8bbdP"Y8 88      88      88 `"8bbdP"Y8  `"YbbdP"Y8  `"YbbdP"'   "Y888 '"Ybbd8"' 88       88 88
-        //             //                                                   aa,    ,88              
-        //             //                                                    "Y8bbdP"              
-        //             //             ";
+            //             Console.ForegroundColor = ConsoleColor.White;
+            //             Console.WriteLine(@"    
+            //             Welcome to...");
+            string text = @"
+                                                                                                  88          88  
+          ,d                                                                      ,d              88         \""
+          88                                                                      88              88";
+            MM88MMM ,adPPYYba, 88,dPYba,,adPYba,  ,adPPYYba,  ,adPPYb,d8  ,adPPYba, MM88MMM ,adPPYba, 88,dPPYba,  88
+          88   \"\"    \`Y8 88P\' \"88\"  \"8a\"\"    `Y8 a8\"   `Y88 a8\"   \"8a  88   a8\"   \"\"88P\'  \"8a 88";
+            88    ,adPPPPP88 88      88      88 ,adPPPPP88 8b       88 8b d8  88   8b         88       88 88";
+          88,   88,    ,88 88      88      88 88,    ,88 "8a,   ,d88 "8a,   ,a8"  88   "8a,   ,aa 88       88 88
+          "Y888 `"8bbdP"Y8 88      88      88 `"8bbdP"Y8  `"YbbdP"Y8  `"YbbdP"'   "Y888 '"Ybbd8"' 88       88 88
+                                                                      aa,    ,88
+                                                                       "Y8bbdP"
+                                ";
 
-        //             Console.WriteLine(text);
-        //         }
-
+            Console.WriteLine(text);
+        }
 
         public static void MainMenu()
         {
